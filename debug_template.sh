24580e9de9389896886286da131bbb578b5b7ae8
Wilson Cheung <info@wilsoncheung.me>
2025-02-26 21:52:34 +0000

    changing to --exclude flag

--- debug_template.sh
+++ debug_template.sh
@@ -6,8 +6,8 @@ copier copy \
     --vcs-ref main \
     --data project_name="lorem-ipsum" \
     --data project_description="Lorem ipsum dolor sit amet." \
-    --skip README.md \
-    --skip debug_template.sh \
+    --exclude README.md \
+    --exclude debug_template.sh \
     ../ ./;
 
 make install;
