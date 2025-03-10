8ebd192c8b1cf987937b8d42dd93e409906e30bb
Wilson Cheung <info@wilsoncheung.me>
2025-03-10 10:37:58 +0000

    temporary updates to project-bass (to be removed)

--- {{ project_name.lower() }}/foo.py
+++ {{ project_name.lower() }}/foo.py
@@ -12,6 +12,20 @@ def foo(bar: str) -> str:
     print("Hello World!")
     return bar
 
+def foozeball(bar: str) -> str:
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        bar: Description of input argument.
+
+    Returns:
+        Description of return value
+    """
+    print("Hello World!")
+    return bar
+
 
 if __name__ == "__main__":  # pragma: no cover
     pass
